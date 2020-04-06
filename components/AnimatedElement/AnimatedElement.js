fda9998eff7551380720b754ccb4b26ede749d63
Harry Li <harryli@Iriss-iMac.local>
2020-04-05 23:33:51 -0400

    function refactor

--- components/AnimatedElement/AnimatedElement.js
+++ components/AnimatedElement/AnimatedElement.js
@@ -11,23 +11,26 @@ export default class AnimatedElement extends React.Component {
     staticProps: PropTypes.object,
     animationType: PropTypes.oneOf(['spring', 'timing', 'decay']),
     animationOptions: PropTypes.object,
-    animationOptions: PropTypes.object,
   }
 
   constructor(props) {
     super(props);
 
-    this.forEachComponentProp((p,i,key) => {
-      this[key] = new Animated.Value(props.animateProps[p]) //set the initial value to the prop
-
-      this[key].addListener( (valueObject) => { //create a listener for this animated value
-        this.myComponentRef.setNativeProps({ [p]: valueObject.value }) //use set native props to update the component
-      });
+    this.forEachGenerateKey(props.animateProps, (p,i,key) => {
+      this.initAnimatedValue(p,i,key,props.animateProps[p])
     })
   }
 
-  forEachComponentProp = forEachFunc => {
-    Object.keys(this.props.animateProps).forEach((p,i) => { //for each component prop
+  initAnimatedValue = (p,i,key,initialValue) => {
+    this[key] = new Animated.Value(initialValue) //set the initial value to the prop
+
+    this[key].addListener( (valueObject) => { //create a listener for this animated value
+      this.myComponentRef.setNativeProps({ [p]: valueObject.value }) //use set native props to update the component
+    });
+  }
+
+  forEachGenerateKey = (obj, forEachFunc) => {
+    Object.keys(obj).forEach((p,i) => { //for each component prop
       const key = "_" + p //get the key
 
       forEachFunc(p,i,key) //run the for each function
@@ -36,8 +39,10 @@ export default class AnimatedElement extends React.Component {
 
 
 
+
+
   componentDidUpdate(prevProps, prevState, snapshot) {
-    this.forEachComponentProp((p,i,key) => {
+    this.forEachGenerateKey(this.props.animateProps, (p,i,key) => {
       if(prevProps.animateProps[p] !== this.props.animateProps[p]) { //if the values are different, we want to animate
         Animated[this.props.animationType]( this[key], { toValue: this.props.animateProps[p], ...this.props.animationOptions } ).start(); //start the animation
       }
