834ea37c253377fb78ba3b01c57a6a4ee1436058
Harry Li <harryli@Iriss-iMac.local>
2020-04-09 22:57:11 -0400

    dictionary entry

--- screens/DictionaryScreen.js
+++ screens/DictionaryScreen.js
@@ -3,6 +3,7 @@ import { StyleSheet, Text, TextInput, TouchableOpacity, View } from 'react-nativ
 import { ScrollView } from 'react-native-gesture-handler';
 import Fuse from 'fuse.js'
 import { withSettings } from "components/Settings/Settings"
+import { withDictionary } from "data/Data"
 
 const options = {
   isCaseSensitive: false,
@@ -12,41 +13,107 @@ const options = {
   useExtendedSearch: false,
   minMatchCharLength: 1,
   shouldSort: true,
-  threshold: 0.6,
+  threshold: 0.8,
   location: 0,
   distance: 100,
-  keys: [],
+  keys: [
+    "0","1","2","3","4"
+  ],
 };
 
-const DictionaryScreen = props => {
-  const [search, onChangeSearch] = React.useState('');
+function getRegex(word) {
+  const allowedSeparator = '\\\s,;"\'|';
 
-  return (
-    <View style={styles.container}>
-      <View style={{position:"relative"}}>
-        <TextInput
-          style={{ height: 40, borderColor: 'gray', borderWidth: 1 }}
-          onChangeText={text => onChangeSearch(text)}
-          value={search}
-          placerholder="Search..."
-        />
-        <Text
-          style={{position:"absolute", top: "50%", right: 10, transform:[{translateY:-10}]}}
-          onPress={e => onChangeSearch("")}
-        >
-          x
-        </Text>
-      </View>
-    </View>
+  const regexStr = `(^.*[${allowedSeparator}]${word}$)|(^${word}[${allowedSeparator}].*)|(^${word}$)|(^.*[${allowedSeparator}]${word}[${allowedSeparator}].*$)`
+  console.log("regexStr ", regexStr)
+  return new RegExp(
+    regexStr,
+    'i', // Case insensitive
   );
 }
 
-export default withSettings(DictionaryScreen)
+class DictionaryScreen extends React.Component {
+  constructor(props) {
+    super(props)
+
+    this.state = {
+      search: "",
+      results: [],
+    }
+  }
+
+  render() {
+    if(this.props.dictionary !== null) {
+      return (
+        <View style={styles.container}>
+          <View style={{position:"relative"}}>
+            <TextInput
+              style={{ height: 40, borderColor: 'gray', borderWidth: 1 }}
+              onChangeText={text => {
+                this.setState({search: text})
+
+                const regex = getRegex(text)
+                const results = []
+                this.props.dictionary.parsed.forEach((p,i) => {
+                  if( regex.test(p[0]) ) { return results.push(i)}
+                  if( regex.test(p[1]) ) { return results.push(i)}
+                  if( regex.test(p[2]) ) { return results.push(i)}
+                  if( regex.test(p[3]) ) { return results.push(i)}
+                  p[4].forEach(e => {
+                    if( regex.test(e) ) { return results.push(i)}
+                  })
+                })
+                console.log("results.length",results.length)
+                this.setState({results})
+              }}
+              value={this.state.search}
+              placerholder="Search..."
+            />
+            <Text
+              style={{position:"absolute", top: "50%", right: 10, transform:[{translateY:-10}]}}
+              onPress={e => onChangeSearch("")}
+            >
+              x
+            </Text>
+          </View>
+
+          <ScrollView>
+            {this.state.results.map((setIndex,i) =>
+              <View key={i} style={styles.entry}>
+                <Text>{this.props.dictionary.parsed[setIndex][0]}</Text>
+                <View>
+                  {this.props.dictionary.parsed[setIndex][4].map((e,j) =>
+                    <Text key={j}>{e}</Text>
+                  )}
+                </View>
+              </View>
+            )}
+          </ScrollView>
+        </View>
+      );
+    }
+
+    return (
+      <View style={{display:"flex", justifyContent:"center", alignItems:"center", height:"100%"}}>
+        <Text style={{fontSize:30}}>Loading...</Text>
+      </View>
+    )
+  }
+}
+
+export default withSettings(
+  withDictionary(DictionaryScreen)
+)
 
 
 const styles = StyleSheet.create({
   container: {
     flex: 1,
     backgroundColor: '#fff',
+  },
+  entry: {
+    padding: 2,
+    borderBottomColor: "gray",
+    borderWidth: 1,
   },
 });
