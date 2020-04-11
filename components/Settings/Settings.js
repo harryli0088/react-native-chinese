6747f854b6ea778f6076450681723f6e023ade9e
Harry Li <harryli@Iriss-iMac.local>
2020-04-11 02:11:29 -0400

    character set restriction dropdown

--- components/Settings/Settings.js
+++ components/Settings/Settings.js
@@ -3,12 +3,28 @@ import { getAsyncStorage, setAsyncStorage } from '../../functions/asyncStorage';
 
 const SettingsContext = React.createContext({settings: {}, setSetting: () => {}});
 
+export const characterSetRestrictions = {
+  "Full Dictionary": [],
+  "HSK1": ["1"],
+  "HSK2 and below": ["1","2"],
+  "HSK2 only": ["2"],
+  "HSK3 and below": ["1","2","3"],
+  "HSK3 only": ["3"],
+  "HSK4 and below": ["1","2","3","4"],
+  "HSK4 only": ["4"],
+  "HSK5 and below": ["1","2","3","4","5"],
+  "HSK5 only": ["5"],
+  "HSK6 and below": ["1","2","3","4","5","6"],
+  "HSK6 only": ["6"],
+}
+
 export default class Settings extends React.Component {
   constructor(props) {
     super(props);
 
     this.state = {
-      traditionalOrSimplified: "traditional"
+      traditionalOrSimplified: "traditional",
+      characterSetRestriction: "Full Dictionary",
     };
   }
 
@@ -19,6 +35,7 @@ export default class Settings extends React.Component {
   getSettingsFromAsyncStorage = async () => {
     this.setState({
       traditionalOrSimplified: await getAsyncStorage('traditionalOrSimplified') === "traditional" ? "traditional" : "simplified",
+      characterSetRestriction: await getAsyncStorage('characterSetRestriction') || this.state.characterSetRestriction,
     })
   }
 
--- screens/SettingsScreen.js
+++ screens/SettingsScreen.js
@@ -6,7 +6,7 @@ import ToggleButtons from 'components/ToggleButtons/ToggleButtons';
 import { withSettings } from "components/Settings/Settings"
 import getLength from "functions/getLength"
 import { extendStart, getPathString } from "functions/geometry"
-
+import { Dropdown } from 'react-native-material-dropdown';
 
 import Svg, {
   G,
@@ -16,7 +16,12 @@ import Svg, {
   ClipPath,
 } from 'react-native-svg';
 
+import { characterSetRestrictions } from "components/Settings/Settings"
+
+const characterSetOptions = Object.keys(characterSetRestrictions).map(value => ({value}))
+
 const SettingsScreen = props => {
+
   return (
     <View style={styles.container}>
       <ScrollView style={styles.container} contentContainerStyle={styles.contentContainer}>
@@ -31,8 +36,10 @@ const SettingsScreen = props => {
           />
         </View>
 
-        <ClipTest
-          traditionalOrSimplified={props.settings.traditionalOrSimplified}
+        <Dropdown
+          label='Restrict the words to'
+          data={characterSetOptions}
+          value={props.settings.characterSetRestriction}
         />
       </ScrollView>
     </View>
@@ -48,72 +55,3 @@ const styles = StyleSheet.create({
     backgroundColor: '#fff',
   },
 });
-
-
-const ClipTest = props => {
-  return (
-    <Svg width={1000} height={1000} style={{background:"#777"}}>
-      <G transform="translate(0,400) scale(0.4,-0.4)">
-        <Defs>
-          <ClipPath id="test">
-            <Path d="M 687 669 Q 718 648 754 623 Q 770 613 786 615 Q 798 618 801 632 Q 802 648 789 678 Q 780 697 746 708 Q 665 726 651 715 Q 647 711 651 697 Q 655 687 687 669 Z"/>
-          </ClipPath>
-        </Defs>
-
-        <Rect x={0} y={0} width={1000} height={1000} fill="pink"></Rect>
-        <AnimatedPathDash
-          value={props.traditionalOrSimplified}
-        />
-      </G>
-    </Svg>
-  )
-}
-
-
-const points = [
-  {x: 657, y: 710},
-  {x: 750, y: 668},
-  {x: 781, y: 634},
-]
-
-const length = getLength(points)
-console.log("length",length)
-const STROKE_WIDTH = 200;
-const pathLength = length + STROKE_WIDTH/2
-console.log("pathLength",pathLength)
-
-const extendedMaskPoints = extendStart(points, STROKE_WIDTH / 2);
-const animationStrokePath = getPathString(extendedMaskPoints)
-console.log("animationStrokePath",animationStrokePath)
-
-
-const AnimatedPath = Animated.createAnimatedComponent(Path);
-
-const AnimatedPathDash = (props) => {
-  const [dashoffsetAnimation] = React.useState(new Animated.Value(pathLength))
-
-  React.useEffect(() => {
-    Animated.timing(
-      dashoffsetAnimation,
-      {
-        toValue: props.value==="traditional"?pathLength:0,
-        duration: 1000,
-      }
-    ).start();
-  }, [props.value])
-
-
-  return (
-    <AnimatedPath
-      clipPath="url(#test)"
-      fill="none"
-      d={animationStrokePath}
-      stroke="green"
-      strokeLinecap="round"
-      strokeLinejoin="miter"
-      strokeWidth={STROKE_WIDTH}
-      strokeDasharray={pathLength + "," + pathLength}
-      strokeDashoffset={dashoffsetAnimation}
-    />
-  );
-}
